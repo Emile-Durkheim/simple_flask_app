1574e4cba1f6aefdc5d6e0a97564ecd3ab57c0f2
Emile Durkheim <emile-durkheim@github.com>
2024-06-12 15:55:12 +0200

    added syslog logging

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,3 @@
 # See https://docs.gunicorn.org/en/stable/deploy.html#nginx-configuration
 access_log_format = '%({x-forwarded-for}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
-
-raw_env = [
-   'IS_PROD=true'
-]
+syslog = True
